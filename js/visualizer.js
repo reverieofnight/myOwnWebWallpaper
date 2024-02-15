c5575a397f06f5ede960c605c2db5c181743e88c
YangHai <esunesea@outlook.com>
2024-02-15 20:50:24 +0800

    feat:优化音频可视化

--- js/visualizer.js
+++ js/visualizer.js
@@ -6,11 +6,9 @@ const visualizerModule = (function(){
     let width = '100'
     let height = '100';
     let ctx = '';
-    let barArr = [];
-    let audioArrayCache = [];
-    let audioArrayDrawCache = [];
     let musicEnd = false;
     let exist = false;
+    let currentAudioArr = [];
     function init(){
         console.log('音频可视化初始化');
         initLayers();
@@ -40,42 +38,81 @@ const visualizerModule = (function(){
                 if(musicEnd){
                     musicEnd = false;
                 }
-                if(audioArrayCache.length === 0){
-                    audioArrayCache.push(audioArray);
-                    requestAnimationFrame(drawBars);
-                } else {
-                    let middleArr = [];
-                    for(let i = 0; i < audioArray.length;i++){
-                       let before = audioArrayCache[audioArrayCache.length - 1][i];
-                       let after = audioArray[i];
-                       let middle = (before + after) / 2;
-                       middleArr.push(middle);
-                    }
-                    audioArrayCache.push(middleArr);
-                    audioArrayCache.push(audioArray);
-                }
+                handleAudioArray(audioArray);
             } else if(!musicEnd){
                 musicEnd = true;
                 ctx.clearRect(0,0,canvas.width,canvas.height);
             }
         }
     }
-    function drawBars(){
-        let arr = audioArrayCache.shift();
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        let barWidth = Math.round(1.0 / 128 * canvas.width);
-        ctx.fillStyle = 'rgb(255,255,255)';
-        for(let i = 0; i < 128; i++){
-            let height = canvas.height * Math.min(arr[i],1);
-            ctx.fillRect(barWidth * i,canvas.height - height,barWidth - 1,height)
+    function handleAudioArray(audioArray){
+        let arr = [];
+        //合并左右声道
+        for(let i = 0;i < 64; i++){
+            arr.push(Math.max(audioArray[i],audioArray[i + 64]));
         }
-        if(audioArrayCache.length > 0){
-            requestAnimationFrame(drawBars);
+        //扩增随机频率
+        let arr2 = addRandomBars(arr,0);
+        //平滑动画并输出
+        smoothAudioArray(arr2);
+    }
+    //增加频率
+    function addRandomBars(arr,time){
+        if(time == 0){
+            return arr;
+        }
+        let arr2 = [];
+        for(let i = 0; i < arr.length;i++){
+            let bar1 = arr[i];
+            let bar3 = arr[i+1] ? arr[i+1] : Math.random() * bar1;
+            let bar2 = Math.random()*(Math.abs(bar1 - bar3)) + Math.min(bar1,bar3);
+            arr2.push(bar1);
+            arr2.push(bar2);
+        }
+        time--;
+        if(time > 0){
+            return addRandomBars(arr2,time);
+        } else {
+            return arr2;
+        }
+    };
+    //平滑动画
+    function smoothAudioArray(audioArray){
+        let step = 0.5;
+        let trip = 0;
+        if(currentAudioArr.length === 0){
+            audioArray.forEach((e) => {
+                currentAudioArr.push(e);
+            })
+        }
+        while(trip < 1){
+            for(let i = 0;i < audioArray.length; i++){
+               if(audioArray[i] > currentAudioArr[i]){
+                currentAudioArr[i] = audioArray[i]
+               } else if(audioArray[i] < currentAudioArr[i]){
+                let diff = audioArray[i] - currentAudioArr[i];
+                currentAudioArr[i] = currentAudioArr[i] + diff * step;
+               }
+            }
+            trip += step;
+            requestAnimationFrame(drawBars(currentAudioArr));
         }
     }
+    //绘制bar
+    function drawBars(arr){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        let barWidth = Math.round(1.0 / arr.length * canvas.width);
+        ctx.fillStyle = 'rgb(255,255,255)';
+        ctx.shadowColor = 'rgba(255,255,255,0.5';
+        ctx.shadowBlur = 10;
+        for(let i = 0; i < arr.length; i++){
+            let height = canvas.height * arr[i] * 0.5;
+            ctx.fillRect(barWidth * i,canvas.height - height,barWidth - 1,height)
+        } 
+    }
+    //销毁
     function destroy(){
         console.log('销毁音频可视化');
-        barArr = [];
         viewer.removeChild(visualizerLayer);
         exist = false;
     }
